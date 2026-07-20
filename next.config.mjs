ce8ac7446184297a0892a666f0683789aab2feb9
Nahidahamedridoy <nahidahamedridoy@gmail.com>
2026-07-20 16:28:37 +0600

    fixed social login

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  serverExternalPackages: ['@better-auth/kysely-adapter', 'kysely'],
+  serverExternalPackages: ['@better-auth/kysely-adapter', 'kysely', 'mongodb'],
   images: {
     remotePatterns: [
       {
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,10 +2,27 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+let clientPromise;
+
+async function getClient() {
+  if (!clientPromise) {
+    const client = new MongoClient(process.env.MONGODB_URI);
+    clientPromise = client.connect();
+  }
+  return clientPromise;
+}
+
+export async function getDb() {
+  const client = await getClient();
+  return client.db(process.env.AUTH_DB_NAME);
+}
+
+const client = await getClient();
 const db = client.db(process.env.AUTH_DB_NAME);
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+
   database: mongodbAdapter(db, {
     client,
   }),
@@ -14,14 +31,15 @@ export const auth = betterAuth({
     enabled: true,
   },
 
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://studymate-ai-nine-nu.vercel.app",
+  ],
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
-    trustedOrigins: [
-      "http://localhost:3000",
-      "https://studymate-ai-nine-nu.vercel.app",
-    ],
   },
 });
